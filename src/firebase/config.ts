dc20d9fb43c5d0f39067c7e4df03d96feae45bd3
Dân Phạm Văn <danpv@lifetex.vn>
2025-11-08 03:24:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4547076944-c5c0d",
+  "appId": "1:131056111087:web:e872aa5d1282af608c3a1a",
+  "apiKey": "AIzaSyAveyreU_eeDq6Y0exJxjz0M3JqXMHlw3U",
+  "authDomain": "studio-4547076944-c5c0d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "131056111087"
+};
