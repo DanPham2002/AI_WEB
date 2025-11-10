6e433b5cfb2746e8b8f190e49cd0a3c0b13e70cf
Dân Phạm Văn <danpv@lifetex.vn>
2025-11-10 02:16:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -29,6 +30,14 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+    ],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1762568025983.cluster-m7dwy2bmizezqukxkuxd55k5ka.cloudworkstations.dev',
     ],
   },
 };
